d50a28e3e338e712d5664c906e723af34473f514
xxsedohrxx <xxsedohrxx@hotmail.com>
2025-11-22 23:20:17 +0100

    Added the ability to make multiple terms in an algebraic equation.

---
+++ Assets/Scripts/AlgebraTerm.cs
@@ -0,0 +1,6 @@
+[System.Serializable]
+public class AlgebraTerm 
+{
+    public int coefficient = 1;
+    public bool hasVariable = true;
+}
--- Assets/Scripts/MathQuestions/AlgebraQuestion.cs
+++ Assets/Scripts/MathQuestions/AlgebraQuestion.cs
@@ -1,22 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Quiz/Questions/Algebra", fileName = "AlgebraQuestion")]
 public class AlgebraQuestion : BaseQuestion
 {
-    [Header("Equation: ax + b = c")]
-    public int a = 1;
-    public int b = 0;
-    public int c = 0;
+    [Header("Equation Terms (Left Side)")]
+    public List<AlgebraTerm> leftSide = new List<AlgebraTerm>();
+
+    [Header("Right Side Constant (Right Side)")]
+    public int rightSide = 0;
 
     public override string GetQuestionText()
     {
-        return $"{a}x + {b} = {c}";
+        string equation = "";
+        for (int i = 0; i < leftSide.Count; i++)
+        {
+            var term = leftSide[i];
+            string part = term.hasVariable ? $"{term.coefficient}x" : $"{term.coefficient}";
+            if (i > 0 && term.coefficient >= 0) equation += " + ";
+            equation += part;
+        }
+        return $"{equation} = {rightSide}";
     }
 
     public override string GetCorrectAnswerText()
     {
-        if (a == 0) return "No solution";
-        float x = (float)(c-b)/a;
+        int xCoeff = 0;
+        int consantSum = 0;
+
+        foreach (var term in leftSide)
+        {
+            if(term.hasVariable)
+                xCoeff += term.coefficient;
+            else
+                consantSum += term.coefficient;
+        }
+
+        if (xCoeff == 0) return "No solution";
+        float x = (float)(rightSide - consantSum)/xCoeff;
         return x.ToString("0.##");
     }
 
